467589be78890012f3c21f613d7c3d86df9f48a3
paulogilvan <paulogilvan74@gmail.com>
2024-03-26 16:15:22 -0300

    adding project urna-eletronica in js vanilla

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '22333',
+        nome: 'Roberval da cruz',
+        partido: 'NHCB',
+        fotos:[
+          {url:'38111.jpg', legenda: 'Vereador'}
+        ]
+      },
+      {
+        numero:'58490',
+        nome: 'Clodorfina Emanuela',
+        partido: 'PMNH',
+        fotos: [
+          {url:'77222.jpg', legenda: 'Vereador'}
+        ] 
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '78',
+        nome: 'Lutador MMA',
+        partido: 'IJKL',
+        vice: 'Luta Livre',
+        fotos: [
+          {url:'99.jpg', legenda: 'Prefeito'},
+          {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: '62',
+        nome: 'Zéca Tatu',
+        partido: 'KLMN',
+        vice: 'Os Barbaros',
+        fotos:[
+          {url:'84.jpg', legenda: 'Prefeito'},
+          {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+    ]
+  }
+];
